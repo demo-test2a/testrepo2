cf587952b9303b278f46b98c80b7186b6c33e3bd
vaani-mohan <41048444+vaani-mohan@users.noreply.github.com>
2019-11-22 10:38:28 +0530

    data1 file 1st commit

---
+++ ec2/maint.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+#Ec2 resource block
+resource "aws_instance" "myec2" {
+  ami = "${var.ami}"
+  instance_type = "${var.instancetype}"
+  subnet_id = "${var.subnetid}"
+  vpc_security_group_ids = ["${var.sgs}"]
+  key_name = "sgkey"
+  availability_zone = "ap-southeast-1a"
+  count = 2
+  ebs_block_device {
+    device_name = "{}"
+    delete_on_termination = "false"
+    volume_id = ""
+    volume_type = "gp2"
+    volume_size = 20
+      }
+  }
+}
